01d2b0d27176e6771e7ff2316a7b3d4477bc409b
TC Brown <trrncbrown@gmail.com>
2026-02-15 22:57:39 -0600

    re-do mod for hub.js

--- static/js/hub.js
+++ static/js/hub.js
@@ -1,7 +1,7 @@
 const portfolioBtn = document.getElementById('app-tile-1');
 
 portfolioBtn.addEventListener('click', () => {
-    window.open("portfolio/index.html");
+    window.location.href = "app/portfolio/index.html";
 })
 const repoBtn = document.getElementById('app-tile-2');
 
